d1b4fbc463d5168152a9824a27ef86926f29ebcb
Lukasz Sliczner <lukaszsliczner@MacBook-Pro.local>
2017-11-16 21:40:49 +0000

    refactor: basic varibles add

---
+++ design.js
@@ -0,0 +1,6 @@
+var gridHeight = $('#gridHeight');
+var gridWidth = $('#gridWidth');
+var gridHeightValue = gridHeight.attr('value'); //return grid height in string
+var gridWidthValue = gridWidth.attr('value'); //return grid width in string
+var button = $('#btnSubmit');
+var colorPicker = $('pickColor');
